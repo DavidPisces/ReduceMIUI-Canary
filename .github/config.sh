ffba110c870ef659a34e7bafc4e64538eb8bf805
泠洛 <42595464+DavidPisces@users.noreply.github.com>
2022-03-18 22:58:55 +0800

    Update config.sh

--- .github/config.sh
+++ .github/config.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-RELEASE_PATH="./release"
 # Config git env
 git config --global user.email "42595464+DavidPisces@users.noreply.github.com "
 git config --global user.name "泠洛"
 # Clone respository
 git clone https://github.com/DavidPisces/ReduceMIUI
 # Clean work directory
-if [ ! -d "$RELEASE_PATH" ]; then
-    mkdir ./release
-else
-    rm -rf release/*
-fi
+mkdir release
+rm -rf release/*
 cd ReduceMIUI
 # Package
 zip -r ReduceMIUI.zip *
